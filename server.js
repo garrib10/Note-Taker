8d444735b4dee21b5ed70625b206ca2887be2026
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-10-22 19:17:43 -0400

    server.js update

--- server.js
+++ server.js
@@ -1,24 +1,16 @@
-const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes")
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+var dbjson = require("./db/db.json");
+
 var app = express();
-
-var PORT = process.env.PORT || 8080;
-
+var PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"))
+app.use(express.static('public'));
 
-app.use("/api", apiRoutes);
-app.use ("/", htmlRoutes)
-
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-  });
-
-
-  app.get('/', function(req,res) {
+app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
@@ -26,50 +18,50 @@ app.get('/notes', function(req,res) {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
-// API Routes// 
 
 app.get('/api/notes', function(req,res) {
-  fs.readFile('./db/db.json', 'utf8', function(err,data) {
-      if (err){
-          throw err;
-      }
-  });
-  res.json(dbjson);
+    fs.readFile('./db/db.json', 'utf8', function(err,data) {
+        if (err){
+            throw err;
+        }
+    });
+    res.json(dbjson);
 
 });
-
 app.post('/api/notes', function(req,res) {  
-  var newpost = req.body;
-  //adds input from notes page to dbjson array of objects
-  function addNewPost(post) {
-      dbjson.push(post);
-      for(let i=0;i<dbjson.length;i++) {
-          dbjson[i].id = i;
-      }
-      return JSON.stringify(dbjson);
-  }
-  fs.writeFile('./db/db.json', addNewPost(newpost), function(err) {
-      if (err){
-          throw err;
-      }
-  });
-  res.json(dbjson);
+    var newpost = req.body;
+   
+    function addNewPost(post) {
+        dbjson.push(post);
+        for(let i=0;i<dbjson.length;i++) {
+            dbjson[i].id = i;
+        }
+        return JSON.stringify(dbjson);
+    }
+    fs.writeFile('./db/db.json', addNewPost(newpost), function(err) {
+        if (err){
+            throw err;
+        }
+    });
+    res.json(dbjson);
 });
 app.delete('/api/notes/:id', function(req,res) {
-  var noteId = req.params.id;
- 
-  dbjson.splice(noteId,1);
-  for(let i=0;i<dbjson.length;i++) {
-      dbjson[i].id = i;
-  }
+    var noteId = req.params.id;
+  
+    dbjson.splice(noteId,1);
 
-  fs.writeFile('./db/db.json', JSON.stringify(dbjson), function(err) {
-      if (err){
-          throw err;
-      }
-  });
-  res.json(dbjson);
+    for(let i=0;i<dbjson.length;i++) {
+        dbjson[i].id = i;
+    }
+    fs.writeFile('./db/db.json', JSON.stringify(dbjson), function(err) {
+        if (err){
+            throw err;
+        }
+    });
+    res.json(dbjson);
 });
+
+
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
