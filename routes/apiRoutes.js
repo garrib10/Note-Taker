293fee84bd37fe97f13536f7a26c81567a6a6f94
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-10-22 18:50:11 -0400

    server update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,11 +11,7 @@ router.post("/notes", (req,res) => {
    Notes.addNotes(req.body).then(notesArray => res.json(notesArray))
 })
 
-// Delete "api/notes"// 
-router.delete("/notes/:id", (req, res) => {
-    // See if the res.json is right or not// 
-    Notes.deleteNotes(req.params.id).then(notesArray => res.json(notesArray))
 
-});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -17,3 +17,59 @@ app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
 
+
+  app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/notes', function(req,res) {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+// API Routes// 
+
+app.get('/api/notes', function(req,res) {
+  fs.readFile('./db/db.json', 'utf8', function(err,data) {
+      if (err){
+          throw err;
+      }
+  });
+  res.json(dbjson);
+
+});
+
+app.post('/api/notes', function(req,res) {  
+  var newpost = req.body;
+  //adds input from notes page to dbjson array of objects
+  function addNewPost(post) {
+      dbjson.push(post);
+      for(let i=0;i<dbjson.length;i++) {
+          dbjson[i].id = i;
+      }
+      return JSON.stringify(dbjson);
+  }
+  fs.writeFile('./db/db.json', addNewPost(newpost), function(err) {
+      if (err){
+          throw err;
+      }
+  });
+  res.json(dbjson);
+});
+app.delete('/api/notes/:id', function(req,res) {
+  var noteId = req.params.id;
+ 
+  dbjson.splice(noteId,1);
+  for(let i=0;i<dbjson.length;i++) {
+      dbjson[i].id = i;
+  }
+
+  fs.writeFile('./db/db.json', JSON.stringify(dbjson), function(err) {
+      if (err){
+          throw err;
+      }
+  });
+  res.json(dbjson);
+});
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
